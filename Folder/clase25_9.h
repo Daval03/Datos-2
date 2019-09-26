5aed1e4ea2daea158d5f0c3bde93671b9ac6b0db
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2019-09-26 00:00:19 -0600

    Create clase25_9.h

---
+++ Folder/clase25_9.h
@@ -0,0 +1,12 @@
+#include <stdint-gcc.h>
+
+class clase25_9_19 {
+public:
+    bool maximo(int izq,int der);
+    int num;
+    clase25_9_19();
+    int fibo(int numero);
+    int fiboDynamic(int numero);
+    int max(int a[num]);
+    int maxaux(int a[num],int inf,int sup);
+};
